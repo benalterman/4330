b1a235fa7ee58e68f7375729f85b1666e8e7692f
jgallegos30 <55601130+jgallegos30@users.noreply.github.com>
2020-03-31 19:37:56 -0500

    Add files via upload

---
+++ database/createAssociation.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+				
+        <title>Maintenance</title>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li><a href="/index.php">Home</a></li>
+                    <li><a href="/reporting">Reporting</a></li>
+                    <li><a href="/searchCommitteeSearch">Search Committee Search</a></li>
+                    <li><a href="/applicantSearch">Applicant Search</a></li>
+                    <li><a href="/maintenance.html">Database Maintenance</a></li>
+                </ul>
+            </div>
+        </nav>
+        
+        <h1 style="margin-left:1%">Association Management</h1>
+		<div class="card text-center">
+			<div class="card-header">
+				<ul class="nav nav-tabs card-header-tabs">
+					<li class="nav-item">
+					  <a class="nav-link" href="/associationManagement.php">Manage Associations</a>
+					</li>
+					<li class="nav-item">
+					  <a class="nav-link active" href="/createAssociation.php">Create New Association</a>
+					</li>
+				</ul>
+			</div>
+			<div class="card-body">
+				<div id="container">
+                    <form action="/insert_association.php" method="post">
+                        <div class=row style="margin-left:1%">
+                            <div class="col-4">
+                                <div class="input-group input-group-sm mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="name">Name</span>
+                                    </div>
+                                    <input type="text" class="form-control" id="name" name="name">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group input-group-sm mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="Description">Description</span>
+                                    </div>
+                                    <input type="text" class="form-control" id="description" name="description">
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="input-group input-group-sm mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="Address">Address</span>
+                                    </div>
+                                    <input type="text" class="form-control" id="address" name="address">
+                                </div>
+                            </div>
+                        </div>
+                        <br/>
+                        <button type="submit" class="btn btn-primary">Create Association</button>
+                    </form>
+                </div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ database/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        
+        <title>Index</title>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="/index.php">Home</a></li>
+                    <li><a href="/reporting">Reporting</a></li>
+                    <li><a href="/searchCommitteeSearch">Search Committee Search</a></li>
+                    <li><a href="/applicantSearch">Applicant Search</a></li>
+                    <li><a href="/maintenance.html">Database Maintenance</a></li>
+                </ul>
+            </div>
+        </nav>
+        <h1 style="margin-left:1%">Welcome!</h1>
+    </body>
+</html>
