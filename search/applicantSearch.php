24300b23f9577efc845dc8fc6d86abab2669ffcb
Benjamin Alterman <balterman94@gmail.com>
2020-04-19 20:24:17 -0500

    update

--- search/applicantSearch.php
+++ search/applicantSearch.php
@@ -2,7 +2,7 @@
   require_once '../database/connect.php';
 
   if (!isset($_POST['/search/applicantSearch']))
-    header("Location: /search/applicantSearch");
+    header("Location: ../search/applicantSearch?error=IncorrectPass");
 
   // create the connection object
   $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
